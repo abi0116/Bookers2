2f30e48663ac89d5e00aaf8d2b78097b177a0e72
abi0116 <kosukeabiru.www@gmail.com>
2021-02-19 02:32:09 +0000

    [add] 4-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @user = current_user
+    @books = Book.all
+    @users = User.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+       redirect_to book_path(@book.id),notice: "successfully"
+    else
+       @user = current_user
+       @books = Book.all
+       render :index
+    end
+  end
+
+  def show
+    @new = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id),notice: "successfully"
+    else
+       render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7119d34a08b7cf6375a636acfc719fa7899a0cf9d305df8aaedd5f6c115a1c77c23c75e5f452583b147d14ab08448541ed9b9d6762e506839aa1d2170ebbc91e'
